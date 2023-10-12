6fd21d1ed4c41263a223313b753c5258bc171aea
SourabhKanase <141937826+SourabhKanase@users.noreply.github.com>
2023-10-12 16:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-
 function decimalToBinary(num) {
-	let arr=[];
-    while(num!=0)
-		{
-			arr.push(num%2);
-			num=Math.floor(num/2);
-		}
-	return arr.reverse();
-  
+  let arr = [];
+  while (num !== 0) {
+    arr.push(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return arr.reverse();
 }
+
 
 window.decimalToBinary = decimalToBinary;
